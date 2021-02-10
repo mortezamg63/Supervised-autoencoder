e4eedf6f928b8e8b3e9fb83ebd6f1b1968b73322
Morteza Mohammady Gharasuie <mortezamg63@gmail.com>
2021-02-10 10:48:41 -0800

    Update SAE_CIFAR10.py

--- SAE_CIFAR10.py
+++ SAE_CIFAR10.py
@@ -28,19 +28,12 @@ class ConvNet(nn.Module):
         self.fc2_1 = nn.Linear(in_features=256,out_features=50176)
         self.conv3 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=(3,3), stride=(1,1))
         self.conv4 = nn.ConvTranspose2d(in_channels=64, out_channels=3, kernel_size=(3,3), stride=(1,1))
-        
-        # self.fc1 = nn.Linear(in_features=2304, out_features=2048)
-        # self.fc2 = nn.Linear(in_features=2048, out_features=512)
-        # self.fc3 = nn.Linear(in_features=512, out_features=128)
-        # self.fc4 = nn.Linear(in_features=128, out_features=32)
+                
         self.softmax = nn.Linear(in_features=32, out_features=10)
         
     def forward(self,x):
-
-        xe = F.relu(self.conv1(x))        
-        # xe = F.max_pool2d(xe, 2, 2)
-        xe = F.relu(self.conv2(xe))
-        # xe = F.relu(self.conv3(xe))
+        xe = F.relu(self.conv1(x))
+        xe = F.relu(self.conv2(xe))        
         shp = [xe.shape[0],xe.shape[1],xe.shape[2],xe.shape[3]]
         xe = xe.view(-1,shp[1]*shp[2]*shp[3])
         xe = F.relu(self.fc1_2(xe))
@@ -55,29 +48,13 @@ class ConvNet(nn.Module):
         
         y_hat = self.softmax(xe)
         
-                
-        # xd = F.relu(self.conv3(latent))
-        # x_hat = F.relu(self.conv4(xd))
-        
-        # x = F.max_pool2d(latent, 2, 2)
-        # x = x.view(-1, 2304)
-        # x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        # x = F.relu(self.fc3(x))
-        # x = F.relu(self.fc4(x))        
-        # y_hat = self.fc5(x)
         return x_hat,y_hat
         
     def name(self):
         return 'lenet'
 
-#arch1: conv32-mp-conv64-[convt32-convt3/mp-f2048-f512-f128-f32-10]
-#arch2: conv32-mp-conv64-[convt32-convt3/mp-f256-10]
-#arch3 conv32-mp-conv64-D256-D32[D32-D256-conv64-up-conv32/D10]
-#arch4 conv32-conv64-D256[D256-conv64-conv32/D10]
-#arch5 conv32-conv64-D256-D32[D32-D256-conv64-conv32/D10]
 def train(weight):
-    path = '/home/morteza/Documents/Dr. Wang/Semi_Supervised_Auto_Encoder-master/models/pytorch/'#curr_DIR = os.path.dirname(os.path.realpath(__file__))+'/models/'#'/home/morteza/Documents/Dr. Wang/Semi_Supervised_Auto_Encoder-master/models/pytorch/'
+    path = './models/pytorch/'#curr_DIR = os.path.dirname(os.path.realpath(__file__))+'/models/'#'/home/morteza/Documents/Dr. Wang/Semi_Supervised_Auto_Encoder-master/models/pytorch/'
     filename = '20epochs-wr'+str(weight)+'-arch5'
     wp=1
     wr=weight
@@ -119,11 +96,7 @@ def train(weight):
             model_acc += num_correct/(len(trainloader)*inputs.size(0))
             running_loss += loss.item()/len(trainloader)
             
-        print("Epoch: ", epoch, " - model_loss: "+ str(running_loss)+' - train_acc: '+str(model_acc))
-        
-        # sys.stdout.write()
-        # sys.stdout.write('\r')
-        # sys.stdout.flush()
+        print("Epoch: ", epoch, " - model_loss: "+ str(running_loss)+' - train_acc: '+str(model_acc))       
             
         # test
         with torch.no_grad():
@@ -135,8 +108,7 @@ def train(weight):
                 x_hat, y_hat = net(images)
                 _,predicted = torch.max(y_hat.data,1)
                 total_images += labels.size(0)
-                test_acc += (predicted==labels).sum().item()/(len(testloader)*images.size(0))
-            # test_acc = total_correct/(total_images * 100
+                test_acc += (predicted==labels).sum().item()/(len(testloader)*images.size(0))            
             print ('Model Test accuracy:',test_acc)
         
         history.append([model_acc,test_acc])    
@@ -152,6 +124,6 @@ def train(weight):
     np.save(path+filename+'-history.npy',history)
         
 
-for weight in [0.04,0.06,0.08]:#range(6,10):
-    w=weight
+for weight in range(1,10):
+    w=weight/10
     train(w)   
